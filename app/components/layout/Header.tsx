d5be407d5444be36e28242a14d56112157195e9c
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-01-19 21:41:31 +0900

    header追加

---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-white/80 dark:bg-gray-900 backdrop-blur-md shadow-md">
+      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-2xl font-bold text-gray-900 dark:text-white"
+        >
+          Signal Photo
+        </Link>
+        <nav className="hidden md:flex space-x-6 text-lg">
+          <Link href="/photos" className="hover:text-gray-300 transition">
+            ホーム
+          </Link>
+          <Link href="/photos/new" className="hover:text-gray-300 transition">
+            新規投稿
+          </Link>
+          <Link href="/register" className="hover:text-gray-300 transition">
+            新規登録
+          </Link>
+          <Link href="/login" className="hover:text-gray-300 transition">
+            ログイン
+          </Link>
+
+          <Link href="/profile" className="">
+            <Image
+              src="/default_icon.png"
+              alt="Profile Avatar"
+              width={32}
+              height={32}
+              className="rounded-full"
+            />
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
