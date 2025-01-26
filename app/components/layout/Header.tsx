e28ff98b4d955b7728f2ca97e3cee6d5e6e8e9ff
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-01-26 15:21:51 +0900

    headerのリファクタリング

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const navigation = [
+  { href: "/photos", label: "ホーム" },
+  { href: "/photos/new", label: "新規投稿" },
+  { href: "/register", label: "新規登録" },
+  { href: "/login", label: "ログイン" },
+];
+
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full bg-white/80 dark:bg-gray-900 backdrop-blur-md shadow-md">
+    <header className="fixed top-0 left-0 w-full bg-white/80 dark:bg-gray-900 backdrop-blur-md shadow-md z-50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <Link
           href="/"
@@ -12,32 +19,16 @@ const Header = () => {
           Signal Photo
         </Link>
         <nav className="hidden md:flex space-x-6 text-lg">
-          <Link
-            href="/photos"
-            className="hover:text-gray-500 dark:text-gray-200 transition"
-          >
-            ホーム
-          </Link>
-          <Link
-            href="/photos/new"
-            className="hover:text-gray-500 dark:text-gray-200 transition"
-          >
-            新規投稿
-          </Link>
-          <Link
-            href="/register"
-            className="hover:text-gray-500 dark:text-gray-200 transition"
-          >
-            新規登録
-          </Link>
-          <Link
-            href="/login"
-            className="hover:text-gray-500 dark:text-gray-200 transition"
-          >
-            ログイン
-          </Link>
-
-          <Link href="/profile" className="">
+          {navigation.map(({ href, label }) => (
+            <Link
+              key={href}
+              href={href}
+              className="hover:text-gray-500 dark:text-gray-200 transition"
+            >
+              {label}
+            </Link>
+          ))}
+          <Link href="/profile">
             <Image
               src="/images/default_icon.png"
               alt="Profile Avatar"
