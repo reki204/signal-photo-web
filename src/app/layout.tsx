0189101c3df60552ce5e2e65cff1a3fba3f1e5bb
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-02-19 21:52:06 +0900

    themaProvider修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          enableColorScheme={false}
           disableTransitionOnChange
         >
           <Header />
