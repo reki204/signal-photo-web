828119a6467e68c5a53b5ff56b374cc7b6865acd
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-03-01 14:03:38 +0900

    resultのpage追加

---
+++ src/app/photos/(result)/page.tsx
@@ -0,0 +1,11 @@
+import { PhotoResultsContainer } from '@/features/photos/containers/PhotoResultContainer';
+
+const ResultPage = () => {
+  return (
+    <>
+      <PhotoResultsContainer />
+    </>
+  );
+};
+
+export default ResultPage;
