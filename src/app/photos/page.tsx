28e40db48ed1a77195d8ec64a9dbc76d67d1f947
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-03-01 14:02:58 +0900

    error.tsxの作成

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,27 +1,18 @@
-import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
-import PhotoResults from '@/features/photos/components/PhotoResults';
-import { PhotoSearchForm } from '@/features/photos/components/PhotoSearchForm';
+import { PhotoSearchContainer } from '@/features/photos/containers/PhotoSearchContainer';
 
-import Loading from './loading';
-
-// const MOCK_PHOTOS = [
-//   { id: '1', url: '/images/Mock/sample1.jpg', userId: '1' },
-//   { id: '2', url: '/images/Mock/sample2.jpg', userId: '1' },
-//   { id: '3', url: '/images/Mock/sample3.jpg', userId: '1' },
-//   { id: '4', url: '/images/Mock/sample2.jpg', userId: '1' },
-//   { id: '5', url: '/images/Mock/sample1.jpg', userId: '1' },
-//   { id: '6', url: '/images/Mock/sample1.jpg', userId: '1' },
-// ];
+import Error from './(result)/error';
+import ResultPage from './(result)/page';
 
 export default function SearchPhotos() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-gray-50 dark:from-slate-900 dark:to-slate-800">
       <div className="container mx-auto px-4 py-16">
-        <PhotoSearchForm />
-        <Suspense fallback={<Loading />}>
-          <PhotoResults />
-        </Suspense>
+        <PhotoSearchContainer />
+        <ErrorBoundary FallbackComponent={Error}>
+          <ResultPage />
+        </ErrorBoundary>
       </div>
     </div>
   );
